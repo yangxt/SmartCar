72540973329f21d08a2e2140add8680ac6f96968
geek-paulwong <wblnumber1@gmail.com>
2014-03-29 17:00:27 -0400

    Successfully implement Houghlines

--- Routes/MainViewController.m
+++ Routes/MainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainViewController.h"
+using namespace cv;
 
 // YOU DEVELOPER'S API KEY ...
 #define API_KEY "2qux5nsev48ur3cm6cgn3msk"
@@ -38,7 +39,7 @@
     videoCamera.defaultAVCaptureVideoOrientation =
     AVCaptureVideoOrientationPortrait;
     videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortraitUpsideDown;
-    videoCamera.defaultFPS = 30;
+    videoCamera.defaultFPS = 5;
     isCapturing = NO;
     
     locationManager = [[CLLocationManager alloc] init];
@@ -68,6 +69,29 @@
 - (void)processImage:(cv::Mat&)image
 {
     // Do some OpenCV processing with the image
+    
+    cv::Mat src = image;
+    cv::Mat dst, cdst;
+    cv::Canny(src, dst, 50, 200, 3);
+    cv::cvtColor(dst, cdst, CV_GRAY2BGR);
+    
+    vector<Vec2f> lines;
+    HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+    
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        cv::Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+    }
+
+    cdst.copyTo(image);
 }
 
 -(void)setRouteLayer:(TTAPIRoutingData *)route {
